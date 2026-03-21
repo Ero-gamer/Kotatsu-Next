fe891b0b9de8413b586624f0e25818b2e3f8ba6e
Error404 <146583787+Ero-gamer@users.noreply.github.com>
2026-03-21 20:55:40 +0100

    Update CoilMemoryCacheKey.kt

--- app/src/main/kotlin/org/koitharu/kotatsu/core/image/CoilMemoryCacheKey.kt
+++ app/src/main/kotlin/org/koitharu/kotatsu/core/image/CoilMemoryCacheKey.kt
@@ -12,37 +12,39 @@ import kotlinx.parcelize.Parcelize
 
 @Parcelize
 class CoilMemoryCacheKey(
-	val data: MemoryCache.Key
+    val data: MemoryCache.Key
 ) : Parcelable {
 
-	companion object : Parceler<CoilMemoryCacheKey> {
-		override fun CoilMemoryCacheKey.write(parcel: Parcel, flags: Int) = with(data) {
-			parcel.writeString(key)
-			parcel.writeInt(extras.size)
-			for (entry in extras.entries) {
-				parcel.writeString(entry.key)
-				parcel.writeString(entry.value)
-			}
-		}
+    companion object : Parceler<CoilMemoryCacheKey> {
+        override fun CoilMemoryCacheKey.write(parcel: Parcel, flags: Int) {
+            parcel.writeString(data.key)
+            parcel.writeInt(data.extras.size)
+            for ((key, value) in data.extras) {
+                parcel.writeString(key)
+                parcel.writeString(value)
+            }
+        }
 
-		override fun create(parcel: Parcel): CoilMemoryCacheKey = CoilMemoryCacheKey(
-			MemoryCache.Key(
-				key = parcel.readString().orEmpty(),
-				extras = run {
-					val size = parcel.readInt()
-					val map = ArrayMap<String, String>(size)
-					repeat(size) {
-						map.put(parcel.readString(), parcel.readString())
-					}
-					map
-				},
-			),
-		)
+        override fun create(parcel: Parcel): CoilMemoryCacheKey {
+            val key = parcel.readString() ?: ""
+            val size = parcel.readInt()
+            val map = ArrayMap<String, String>(size)
+            repeat(size) {
+                val eKey = parcel.readString()
+                val eValue = parcel.readString()
+                if (eKey != null && eValue != null) {
+                    map[eKey] = eValue
+                }
+            }
+            return CoilMemoryCacheKey(MemoryCache.Key(key, map))
+        }
 
-		fun from(view: View): CoilMemoryCacheKey? {
-			return (CoilUtils.result(view) as? SuccessResult)?.memoryCacheKey?.let {
-				CoilMemoryCacheKey(it)
-			}
-		}
-	}
+        fun from(view: View): CoilMemoryCacheKey? {
+            // Coil 3 returns a specific Result type; we safe cast to SuccessResult
+            val result = CoilUtils.result(view)
+            return (result as? SuccessResult)?.memoryCacheKey?.let {
+                CoilMemoryCacheKey(it)
+            }
+        }
+    }
 }
